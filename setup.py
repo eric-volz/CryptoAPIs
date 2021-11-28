62c2d0289b423d2cb81611ab9261537693d37ad9
eric-volz <70062781+eric-volz@users.noreply.github.com>
2021-11-28 13:48:07 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,11 @@
+from distutils.core import setup
 
+setup(name="CryptoAPIs",
+      version="1.0.0",
+      description="Get's data from many exchanges",
+      license='MIT',
+      author="ev",
+      authoer_email="eric@volz.link",
+      url='https://github.com/eric-volz/CryptoAPIs',
+      packages=['CryptoAPIs']
+)
